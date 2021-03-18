01cc8de2bc24e0c5c35d46f698e2f56366f4a088
dsenkin <denisx9.0c@gmail.com>
2021-03-18 11:44:01 +0100

    fix for mnist

--- src/optim/deepSVDD_trainer.py
+++ src/optim/deepSVDD_trainer.py
@@ -87,7 +87,7 @@ class DeepSVDDTrainer(BaseTrainer):
                 inputs.requires_grad_(False)
                 #if r is None:
                 #    r = torch.randn((1,) + grads.shape[1:], device=self.device)
-                print(outputs.shape, self.c.shape, grads.shape, self.c_g.expand_as(grads).shape)
+                #print(outputs.shape, self.c.shape, grads.shape, self.c_g.expand_as(grads).shape)
                 dist = torch.sum((outputs - self.c) ** 2, dim=1)
                 dist1 = (grads - self.c_g.expand_as(grads))**2
                 #dist = torch.sum((outputs - self.c) ** 2, dim=1)
